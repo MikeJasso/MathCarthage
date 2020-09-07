62c560478cb10ec5e5e162758a0c03db03950419
Miguel Angel Jasso Estrada <miguel.jasso@payclip.com>
2020-09-06 23:11:06 -0500

    Carthage

---
+++ MathCarthage/Carthage/Build/iOS/MathCarthage.framework/Headers/MathCarthage.h
@@ -0,0 +1,19 @@
+//
+//  MathCarthage.h
+//  MathCarthage
+//
+//  Created by MiguelJasso on 06/09/20.
+//  Copyright © 2020 xp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MathCarthage.
+FOUNDATION_EXPORT double MathCarthageVersionNumber;
+
+//! Project version string for MathCarthage.
+FOUNDATION_EXPORT const unsigned char MathCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MathCarthage/PublicHeader.h>
+
+
